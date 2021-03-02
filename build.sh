e46fc6f83c54b56014363323774cbc8a30eb56b6
Blanca P. Olesnevich <bolesnevich@blockringtm.gq>
2021-03-03 00:20:05 +0100

    debug netlify CI/CD

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #
 
+keyid=${IPNS_KEYID:-QmaNjQKyUuheZPMpL6qXGzSAXFiLx2vsnr6ipHvs968KDz}
+curl -sL https://cloudflare-ipfs.com/ipns/$keyid/info/refs
+
 top=$(git rev-parse --show-toplevel)
 cd $top
-git pull origin master
+git remote -v
+git config pull.rebase false 
+git pull
 
 www=public
 git submodule deinit -f $www
@@ -11,6 +16,6 @@ rm -rf $gitdir/modules/$www
 
 git checkout $top/.gitmodules
 git submodule update --init --remote --recursive $www
-remote=$(git -C dist remote get-url origin) && echo origin: $remote
+remote=$(git -C $www remote get-url origin) && echo remote: $remote
 curl -L $remote/refs/heads/master
 
