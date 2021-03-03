4ff1bee06e821aa027c3f623997e52a62d677519
Camille D. Jonte <cjonte@hltm.ml>
2021-03-03 15:26:31 +0100

    an other version again !

--- once.sh
+++ once.sh
@@ -3,7 +3,7 @@ echo once.sh ...
 set -x
 
 rm -f index.html
-url=$(grep url .gitmodules | head -1 | sed -e 's/^  *url *= *//')
+url=$(grep url .gitmodules | head -1 | sed -e 's/[ \t][ \t]*//g' -e 's/^url=//')
 echo url: $url
 git remote set-url --add origin "$url"
 
