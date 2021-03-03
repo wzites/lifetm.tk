d73588c4bd5ebc0aac38ffd94329f1f737291711
Derrick E. Mentis <dmentis@happycoin.gq>
2021-03-03 08:57:26 +0100

    testing local build

--- build.sh
+++ build.sh
@@ -2,25 +2,25 @@
 
 pwd
 ls -la
+repo_name=lifetm.tk.git
 www=public
+keyid=${IPNS_KEYID:-QmaNjQKyUuheZPMpL6qXGzSAXFiLx2vsnr6ipHvs968KDz}
+#echo info/refs:
+#curl --connect-timeout 2 --max-time 30 -sL https://ipfs.blockring™.ml/ipns/$keyid/$reponame/info/refs
 
 top=$(git rev-parse --show-toplevel) && echo top: $top
 cd $top
-git remote -v
+echo -n "origin_url: "
+git remote get-url origin
 echo gitmodules:
 cat .gitmodules
 echo .
 
 git log -1 \
---pretty=format:'--- # git-commit-data%ncommit: "%H"%nauthor: "%an <%ae>"%ndate: "%ad"%nmessage: "%s"%n' | tee git-info.yml
+--pretty=format:'--- # git-commit-data%ncommit: "%H"%nauthor: "%an <%ae>"%ndate: "%ad"%nmessage: "%s"%n'
 
-keyid=${IPNS_KEYID:-QmaNjQKyUuheZPMpL6qXGzSAXFiLx2vsnr6ipHvs968KDz}
 repo_url=$(git -C $www remote get-url origin) && echo repo_url: $repo_url
 symb=${repo_url##*/} && echo symb: $symb
-echo info/refs:
-curl --connect-timeout 2 --max-time 30 -sL https://ipfs.blockring™.ml/ipns/$keyid/$symb/info/refs
-echo .
-
 
 #git submodule deinit -f $www
 #gitdir=$(git rev-parse --git-dir) && echo gitdir: $gitdir
@@ -29,10 +29,11 @@ echo .
 set -x
 #git config pull.rebase false 
 git checkout $top/.gitmodules
-git submodule update --init --remote --recursive $www
+#git submodule update --init --remote --recursive $www
+git submodule update --remote $www
 set +x
 
-remote=$(git -C $www remote get-url origin) && echo remote: $remote
+remote_url=$(git -C $www remote get-url origin) && echo remote_url: $remote_url
 
 git -C $www log -1
 #curl --connect-timeout 2 --max-time 30 -L $remote/refs/heads/master
