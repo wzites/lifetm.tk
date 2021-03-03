e027abd7ca2e2bd9c89b80b869a9709a2de16d1b
Wendell J. Piorkowski <wpiorkowski@gctm.gq>
2021-03-03 15:39:55 +0100

    ...

--- once.sh
+++ once.sh
@@ -1,13 +1,18 @@
 #
 echo once.sh ...
-set -x
 
 rm -f $www/index.html
 url=$(grep url .gitmodules | head -1 | sed -e 's/[ \t][ \t]*//g' -e 's/^url=//')
 echo url: $url
+set -x
  git -C $www remote set-url --delete origin $repo_url
  git -C $www remote set-url --add origin "$url"
-
+ git -C $www remote -v
 set +x
+
+ echo "$www's config:"
+ cat $(git rev-parse --git-dir)/config
+ echo .
+
 
 true;
