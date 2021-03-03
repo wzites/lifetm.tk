feb93c3bb36816ef11c20b2edd2faa88c9df716f
Blanca P. Olesnevich <bolesnevich@blockringtm.gq>
2021-03-03 06:44:48 +0100

    changes build.sh to avoid time overun

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #
 
+set -xe
 keyid=${IPNS_KEYID:-QmaNjQKyUuheZPMpL6qXGzSAXFiLx2vsnr6ipHvs968KDz}
-curl -sL https://cloudflare-ipfs.com/ipns/$keyid/info/refs
+curla --connect-timeout 2 --max-time 30 -sL https://ipfs.blockring™.ml/ipns/$keyid/info/refs
 
-top=$(git rev-parse --show-toplevel)
+top=$(git rev-parse --show-toplevel) && echo top: $top
 cd $top
 git remote -v
-git config pull.rebase false 
-git pull
+#git config pull.rebase false 
 
 www=public
-git submodule deinit -f $www
-gitdir=$(git rev-parse --git-dir)
-rm -rf $gitdir/modules/$www
+#git submodule deinit -f $www
+#gitdir=$(git rev-parse --git-dir) && echo gitdir: $gitdir
+#rm -rf $gitdir/modules/$www
 
 git checkout $top/.gitmodules
 git submodule update --init --remote --recursive $www
 remote=$(git -C $www remote get-url origin) && echo remote: $remote
+
+git -C $www log -1
 curl -L $remote/refs/heads/master
 
+exit $?;
+true
