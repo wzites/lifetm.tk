a0c65640a37703c58279075449a41f0eb25242d2
Sonja J. Zavcedo <szavcedo@keysm.ml>
2021-03-03 15:19:18 +0100

    fixing once.sh

--- once.sh
+++ once.sh
@@ -1,10 +1,11 @@
 #
+echo once.sh ...
 set -x
 
-git remote rm hologit
-url=$(grep url .gitmodules | head -1 | sed -e 's/^  *url=//')
+rm -f index.html
+url=$(grep url .gitmodules | head -1 | sed -e 's/^  *url *= *//')
 echo url: $url
-git remote seturl --add origin $url
+git remote set-url --add origin "$url"
 
 set +x
 
